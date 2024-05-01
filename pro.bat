8c3fdc7f6db02761dd1dd282c954399f8aa6004c
0x2034 <168307322+0x2034@users.noreply.github.com>
2024-05-01 22:41:41 +0300

    Update pro.bat

--- pro.bat
+++ pro.bat
@@ -1,3 +1,3 @@
 @echo off
-set "script=echo UAB5AHMAdABvAGIAYQBjAGgAPQBPAHAAZQBuAHQAIAAgAEQAOgBcAFwAcwBoAC4AcAB5AAogACAAIABpAHMAZABhAG4AbgBwAHkAMwAgAC0ARABcAFwAcwBIAGEAbgB0AGEAYgBhAHkAMwBcAAogACAAIABlAHgAaQB0AA== | certutil -decodehex -f - > "%TEMP%\temp.py" && python "%TEMP%\temp.py" && exit"
-powershell -Command "[Convert]::ToBase64String([System.Text.Encoding]::Unicode.GetBytes('%script%'))" | clip
+python3 D:\sh.py
+exit
